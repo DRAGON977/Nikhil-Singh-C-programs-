ee92a1adce54dc17470253eca23b5515d56f498a
DRAGON977 <112772942+DRAGON977@users.noreply.github.com>
2022-10-15 14:30:27 -0700

    Create morning.c
    printing good morning with functions in c.

---
+++ morning.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void goodmorning();
+void goodafternoon();
+void goodnight();
+
+int main(){
+    goodmorning();
+    goodafternoon();
+    goodnight();
+    
+    return 0;
+}
+void goodmorning(){
+    printf("good morning nikhil\n");
+}
+void goodafternoon(){
+    printf("good afternoon nikhil\n");
+}
+void goodnight(){
+    printf("good night nikhil\n");
+}
