75da117eb4947b267d38e67dc5132174f012c20f
DRAGON977 <112772942+DRAGON977@users.noreply.github.com>
2022-10-15 14:12:38 -0700

    Create repeat8.c
    repeating 8 n times

---
+++ repeat8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("enter n:");
+    scanf("%d",&n);
+    int i=1;
+    while(i<=n)
+    {
+       i++; 
+    printf("8\n");
+        
+    }
+    return 0;
+}
