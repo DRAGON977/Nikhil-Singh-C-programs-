16532e3bfe15e3a82a6ef6455d2001749ba2ede3
DRAGON977 <112772942+DRAGON977@users.noreply.github.com>
2022-10-15 14:29:25 -0700

    Create helloinfuntion.c
    printing hello multiple times.

---
+++ helloinfuntion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void printa();
+int main()
+{
+    int n;
+    printf("enter n:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        printa();
+    }
+   /* printa();
+    printa();
+    printa();
+    printa();*/
+
+    return 0;
+}
+void printa()
+{
+    printf("fir milenge chalte chalte\n");
+
+    printf("aur bhai kaise ho\n");
+}
