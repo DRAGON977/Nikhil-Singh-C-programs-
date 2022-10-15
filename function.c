665c533693bef62a26131945009dcdf54781c621
DRAGON977 <112772942+DRAGON977@users.noreply.github.com>
2022-10-15 14:28:16 -0700

    Create function.c
    explaining functions in c.

---
+++ function.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void display(){
+    printf("hello nikhil\n");//we can assign or define function just after function prototype.
+} // funtion prototype:function ban gya.
+
+int main()
+{
+    printf("function starting its work\n");
+    display(); // function call:functon main me aagya.
+    printf("function finished its work\n");
+
+    return 0;
+}
+/*void display()
+{
+    printf("hello nikhil\n"); // function definition:function se aap kya karana cah rhe h.
+}*/
