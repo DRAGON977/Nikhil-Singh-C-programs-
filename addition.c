8263f03b4e9d0160417085fed5e1a2d44c665125
DRAGON977 <112772942+DRAGON977@users.noreply.github.com>
2022-10-15 13:54:02 -0700

    Create addition.c
    addition of two numbers

---
+++ addition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+  int x, y, z;
+
+  printf("Enter first number:-\n");
+  scanf("%d",&x);
+  printf("enter second number:-\n");
+  scanf("%d",&y);
+
+  z = x + y;
+
+  printf("Sum of the numbers = %d\n", z);
+
+  
+}
