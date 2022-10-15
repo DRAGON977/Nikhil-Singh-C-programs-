cd4c6db6d72c8d9aa189e4a42c43141c1fd39901
DRAGON977 <112772942+DRAGON977@users.noreply.github.com>
2022-10-15 14:05:03 -0700

    Create conditional2.c
    ||(or)conditional operator

---
+++ conditional2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+   {
+      int c;
+    printf("enter a character:");
+    scanf("%d",&c);
+    if(c==0||c==1||c==2||c==3||c==4||c==5||c==6||c==7||c==8||c==9)
+    printf("it is a digit");
+    else printf("it is not a digit");
+    return 0;
+    } 
