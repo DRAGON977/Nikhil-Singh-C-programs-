641bf42750d4fe3f199ee6b6203bb0a303fff55c
DRAGON977 <112772942+DRAGON977@users.noreply.github.com>
2022-10-16 11:16:21 -0700

    Create array2.c

---
+++ array2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//is a[0] works?
+
+int main(){
+    int a[0];
+    printf("enter phy marks:");
+    scanf("%d",&a[0]);
+    printf("\nmarks=%d",a[0]);
+    printf("\nenter che marks:");
+    scanf("%d",&a[1]);
+    printf("\nmarks=%d",a[1]);
+    return 0;
+} 
