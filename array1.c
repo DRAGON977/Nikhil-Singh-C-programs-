9852efedf5a3008e212cbdb7758ac5eeb2e33acd
DRAGON977 <112772942+DRAGON977@users.noreply.github.com>
2022-10-16 11:15:52 -0700

    Create array1.c
    defining array in c.

---
+++ array1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int marks[3];
+    printf("enter phy marks:");
+    scanf("%d",&marks[0]);
+    printf("enter chem marks:");
+    scanf("%d",&marks[1]);  
+    printf("enter maths marks:");
+    scanf("%d",&marks[2]);
+    printf("phy=%d\nchem=%d\nmaths=%d",marks[0],marks[1],1[marks]);
+    
+    
+    return 0;
+}
